67c38d01798ba68aa41c4870d087209ac0f05c4a
tpnovaldev <tubagusputranovaldi@gmail.com>
2022-11-21 04:00:06 +0700

    initial commit

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,17 @@
 module.exports = function (api) {
   api.cache(true)
 
-  const presets = ["@babel/preset-env", "@babel/preset-react"]
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        "targets": [">0.25%", "not ie 11"],
+        "debug": true,
+        "useBuiltIns": "usage",
+      }
+    ],
+    "@babel/preset-react"
+  ]
 
   return {
     presets
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/main.scss'
 
-const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.render(<App />, rootElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
