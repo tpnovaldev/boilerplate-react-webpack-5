8007b3f2e96d9ff6a388c5e7aae5d17405a82e74
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-21 11:22:42 +0700

    feat: added onclick props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Button = ({ children }) => {
+export const Button = ({ children, ...rest }) => {
   return (
     <>
-      <button style={{ marginTop: '24rem' }}>{children}</button>
+      <button style={{ marginTop: '24rem' }} {...rest}>{children}</button>
     </>
   )
 }
