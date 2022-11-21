c481a269dcefa103ba5a2d4becdf389c6ca7e036
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-21 12:56:28 +0700

    feat: added eslint configuration

--- .babelrc.js
+++ .babelrc.js
@@ -3,17 +3,30 @@ module.exports = function (api) {
 
   const presets = [
     [
-      "@babel/preset-env",
-      {
-        "targets": [">0.25%", "not ie 11"],
-        "debug": true,
-        "useBuiltIns": "usage",
+      // Transform ES6+ into valid ES5 code
+      '@babel/preset-env', {
+        'modules': false,
+        'targets': {
+          'browsers': [
+            'last 2 Chrome versions',
+            'last 2 Firefox versions',
+            'last 2 Safari versions',
+            'last 2 iOS versions',
+            'last 1 Android version',
+            'last 1 ChromeAndroid version',
+            'ie 11'
+          ]
+        }
       }
     ],
-    "@babel/preset-react"
-  ]
+    '@babel/preset-react' // Extends Babel support to JSX
+  ];
+  const plugins = [
+    '@babel/plugin-proposal-class-properties' // Use properties directly on a class
+  ];
 
   return {
-    presets
-  }
-}
+    presets,
+    plugins
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,68 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
-  return {
+  const configs = {
     entry: './src/index.js',
-    devtool: 'source-map', // created a source map
+    devtool: argv.mode === 'production' ? 'source-map' : 'inline-source-map', // Created a source map
     output: {
       // Source files for production server
       path: path.resolve(__dirname, 'dist'),
+      // publicPath: '/',
       filename: '[name].bundle.js',
       assetModuleFilename: (pathData) => {
-        const filepath = path.dirname(pathData.filename).split("/").slice(1).join("/");
+        const filepath = path.dirname(pathData.filename).split('/').slice(1).join('/');
         return `${filepath}/[name].[hash][ext][query]`;
       },
       clean: true,
+    },
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.js', '.jsx'],
     },
     devServer: {
       // Running source files in development server
       static: path.join(__dirname, 'dist'),
       port: 3000,
       open: true,
-      hot: true
+      hot: true,
     },
     module: {
       rules: [
         {
-          // Compile ES6+ JavaScript into ES5
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader', // Transform JSX into normal JavaScript
+            loader: 'babel-loader', // Transpile files with Babel and webpack
             options: {
-              sourceMap: true
-            }
-          }
+              sourceMap: true,
+            },
+          },
         },
         {
           // Convert SCSS files into CSS file
           test: /\.(scss|css)$/,
           use: [
-            { loader: argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader' }, // Injects CSS into the DOM using multiple <style></style> and works faster
-            { loader: 'css-loader', options: { sourceMap: true } },
-            { loader: 'postcss-loader', options: { sourceMap: true } },
-            { loader: 'sass-loader', options: { sourceMap: true } }
-          ]
+            { loader: argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader' }, // Inject CSS into the DOM
+            { loader: 'css-loader', options: { sourceMap: true } }, // Resolve CSS imports
+            { loader: 'postcss-loader', options: { sourceMap: true } }, // Load SCSS and compile to CSS
+            { loader: 'sass-loader', options: { sourceMap: true } }, // Load SCSS and compile to CSS
+          ],
         },
         {
-          // Optimize (compress) all images 
+          // Optimize (compress) all images
           test: /\.(jpe?g|png|gif|svg)$/i,
           type: 'asset',
         },
-      ]
+      ],
     },
     optimization: {
       minimize: true, // Minimize files in development as well
@@ -99,17 +106,21 @@ module.exports = (env, argv) => {
               ],
             },
           },
-        }) // Optimize (compress) all images
-      ]
+        }), // Optimize (compress) all images
+      ],
     },
     plugins: [
       new NodePolyfillPlugin(),
       new HtmlWebpackPlugin({
-        template: './index.html',
+        title: 'Boilerplate React Webpack 5',
+        favicon: './public/favicon.png',
+        template: './public/index.html',
         filename: 'index.html',
-        inject: 'body',
       }), // Create HTML files to serve your webpack bundles
+      new ESLintPlugin(),
       ...(argv.mode === 'production' ? [new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[id].css' })] : []), // Extracts CSS into separate files
-    ]
-  }
-}
+    ],
+  };
+
+  return configs;
+};
