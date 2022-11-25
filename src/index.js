bfb9dff3ef6aa0bb6204cdee194d9ace2989e10e
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-25 16:00:54 +0700

    feat: initialized routing web app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import './styles/main.scss';
 
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+
+  React.useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return children;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Suspense>
+    <BrowserRouter>
+      <Wrapper>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Wrapper>
+    </BrowserRouter>
   </React.StrictMode>,
 );
