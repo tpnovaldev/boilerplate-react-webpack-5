3fa4014016319ac4f0a4ba5997cb52f968c926f4
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-25 15:56:48 +0700

    feat: added container component

---
+++ src/components/atoms/Container/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Container({ children }) {
+  return (
+    <div className="container">
+      {children}
+    </div>
+  );
+}
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Container;
