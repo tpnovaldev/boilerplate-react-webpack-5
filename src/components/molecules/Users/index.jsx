ad9411204fc89248e1864dbc1da8f2a8c48aaaad
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-25 15:57:24 +0700

    feat: added users components

---
+++ src/components/molecules/Users/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Users.module.scss';
+
+function Users({ loading, error, data }) {
+  // Loading user state
+  if (loading) return <p>Loading please wait...</p>;
+
+  // Error user state
+  if (error) return <p>Users not available for this time</p>;
+
+  return (
+    <ul className={styles.users}>
+      {data.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+Users.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.array.isRequired,
+};
+
+export default Users;
