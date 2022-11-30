c1626e7562baed06b3dcfcf4c02fbe12218931bd
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-30 12:01:26 +0700

    feat: added base layout component

---
+++ src/components/templates/BaseLayout/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../../organisms/Header';
+import styles from './BaseLayout.module.scss';
+
+const BaseLayout = ({ children }) => (
+  <div className={styles.wrapper}>
+    <Header />
+    <main>
+      {children}
+    </main>
+  </div>
+)
+
+BaseLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default BaseLayout;
