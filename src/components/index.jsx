460ed069d5984ac29701b27ca188b962cf8837b5
Tubagus Putra <tubagusputranovaldi@gmail.com>
2022-11-30 11:52:54 +0700

    feat: reinitialized components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-// Atoms Components
+// Atom Components
 export const Brand = React.lazy(() => import(/* webpackChunkName: "brand" */ './atoms/Brand'));
 export const Button = React.lazy(() => import(/* webpackChunkName: "button" */ './atoms/Button'));
 export const Container = React.lazy(() => import(/* webpackChunkName: "container" */ './atoms/Container'));
 export const Typography = React.lazy(() => import(/* webpackChunkName: "typography" */ './atoms/Typography'));
-// Molecules Components
+// Molecule Components
+export const Navigation = React.lazy(() => import(/* webpackChunkName: "navigation" */ './molecules/Navigation'));
 export const Users = React.lazy(() => import(/* webpackChunkName: "users" */ './molecules/Users'));
-// Organisms Components
+// Organism Components
+export const Header = React.lazy(() => import(/* webpackChunkName: "header" */ './organisms/Header'));
 export const Modal = React.lazy(() => import(/* webpackChunkName: "modal" */ './organisms/Modal'));
 export const RangeSlider = React.lazy(() => import(/* webpackChunkName: "rangeslider" */ './organisms/RangeSlider'));
 export const SpinWheel = React.lazy(() => import(/* webpackChunkName: "spinwheel" */ './organisms/SpinWheel'));
+// Template Components
+export const BaseLayout = React.lazy(() => import(/* webpackChunkName: "baselayout" */ './templates/BaseLayout'));
